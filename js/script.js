cbc6b7676951450dac302e9fc0fd00c7eab1a4a9
Chris Jonathan <christophereko@hotmail.fr>
2018-11-10 19:16:00 +0000

    The calculator is more or less done minus some minor issues which should be resolved soon. I hope.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,112 @@
+let zeroBtn = document.getElementById('calc-zero');
+let oneBtn = document.getElementById('calc-one');
+let twoBtn = document.getElementById('calc-two');
+let threeBtn = document.getElementById('calc-three');
+let fourBtn = document.getElementById('calc-four');
+let fiveBtn = document.getElementById('calc-five');
+let sixBtn = document.getElementById('calc-six');
+let sevenBtn = document.getElementById('calc-seven');
+let eightBtn = document.getElementById('calc-eight');
+let nineBtn = document.getElementById('calc-nine');
 
+let decimalBtn = document.getElementById('calc-decimal');
+let clearBtn = document.getElementById('calc-clear');
+let backspaceBtn = document.getElementById('calc-backspace');
+let displayValElement = document.getElementById('calc-display-val');
+
+let displayVal = '0';
+let pendingVal;
+let evalStringArray = [];
+
+let calcNumBtns = document.getElementsByClassName('calc-btn-num');
+let calcOperatorBtns = document.getElementsByClassName('calc-btn-operator');
+
+function updateDisplayVal (e) {
+    let btnText = e.target.innerText;
+    if(displayVal === '0') {
+        displayVal = '';
+    }
+    displayVal += btnText;
+    displayValElement.innerText = displayVal;
+}
+
+function performOperation (e) {
+    let operator = e.target.innerText;
+
+    switch (operator) {
+        case '+':
+            pendingVal = displayVal;
+            displayVal = '0';
+            displayValElement.innerText = displayVal;
+            evalStringArray.push(pendingVal);
+            evalStringArray.push('+');
+        break;
+
+        case '-':
+            pendingVal = displayVal;
+            displayVal = '0';
+            displayValElement.innerText = displayVal;
+            evalStringArray.push(pendingVal);
+            evalStringArray.push('-');
+        break;
+
+        case 'x':
+            pendingVal = displayVal;
+            displayVal = '0';
+            displayValElement.innerText = displayVal;
+            evalStringArray.push(pendingVal);
+            evalStringArray.push('*');
+        break;
+
+        case '÷':
+            pendingVal = displayVal;
+            displayVal = '0';
+            displayValElement.innerText = displayVal;
+            evalStringArray.push(pendingVal);
+            evalStringArray.push('/');
+        break;
+
+        case '=':
+            evalStringArray.push(displayVal);
+            let evaluation = eval(evalStringArray.join(' '));
+            displayVal = evaluation + '';
+            displayValElement.innerText = displayVal;
+            evalStringArray = [];
+        break;
+
+        default:
+        break;
+    }
+}
+
+for(let i = 0; i < calcNumBtns.length; i++) {
+    calcNumBtns[i].addEventListener('click', updateDisplayVal, false);
+}
+
+for(let i = 0; i < calcOperatorBtns.length; i++) {
+    calcOperatorBtns[i].addEventListener('click', performOperation, false);
+}
+
+clearBtn.addEventListener('click', () => {
+    displayVal = '0';
+    pendingVal = undefined;
+    evalStringArray = [];
+    displayValElement.innerTEXT = displayVal;
+}, false);
+
+backspaceBtn.addEventListener('click', () => {
+    let lengthOfDisplayVal = displayVal.length;
+    displayVal = displayVal.slice(0, lengthOfDisplayVal - 1);
+
+    if(displayVal === '') {
+        displayVal = '0';
+    }
+    displayValElement.innerText = displayVal;
+}, false);
+
+decimalBtn.addEventListener('click', () => {
+    if(!displayVal.includes('.')) {
+        displayVal += '.';
+        displayValElement.innerText = displayVal;
+    }
+}, false);
